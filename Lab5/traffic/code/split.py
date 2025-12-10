3dc0728f6d8f41c1e1dd2a64cca5e1708bad9ba3
ale0xc <acachera59@gmail.com>
2025-12-10 21:04:26 +0100

    split dataset traffic

---
+++ Lab5/traffic/code/split.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from data_loader import load_data
+
+def series_train_test_split(data, trn_pct: float = 0.90):
+    trn_size = int(len(data) * trn_pct)
+    df_cp = data.copy()
+    train = df_cp.iloc[:trn_size, 0]
+    test = df_cp.iloc[trn_size:, 0]
+    return train, test
+
+def split_data(
+    target_column: str = 'Total', 
+    train_size: float = 0.3, 
+):
+    """
+    Splits the dataset into training and testing sets.
+    """
+    print(f"--- Splitting data with train size = {train_size} ---")
+
+    df = load_data()
+    timeseries = df[target_column]
+    
+    train, test = series_train_test_split(timeseries, trn_pct=train_size)
+    
+    print(f"Data split completed: {len(train)} training samples and {len(test)} testing samples.")
+    
+    return train, test
